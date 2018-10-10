6409e3c0282f5415cf48f99ba58ee5eeace3d759
Наталья <natakhatina@gmail.com>
2018-10-10 20:12:06 +0300

    Домашка

--- string_script.py
+++ string_script.py
@@ -1,17 +1,14 @@
-#a='Сегодня 4 октября'
-# for i=0:9
-#     if a.find('i')>0:
-#         a.replace('i','')
+import random,string
+# УБРАТЬ ЦИФРЫ ИЗ СТРОКИ
+# a='Сегодня 4 октября'
+# print(a)
+# for i in range(0,10):
+#     i=str(i)
+#     if a.find(i)>0:
+#         a=a.replace(i,'')
+# print(a)
 
-# if a.find('4')>0:
-#     a.replace('4','')
-#     print(a)
-
-# b=a.find('4')
-# print(b)
-# a=a.replace('4','')
-# print (a)
-
+# УБРАТЬ ЗАГЛАВНЫЕ БУКВЫ ИЗ СТРОКИ
 # b='abcVgT7J' #первый вариант
 # print(b)
 # for e in b:
@@ -30,10 +27,25 @@
 #
 # print(b)
 
-a='cajslUhsRw2/fwse68FLp3j/42372/j?kiOO'
-print(a)
-b=a.split('/')
+n=6
+r=random.randint(n+1,50)
+b=''
+b=b.join(random.choice(string.printable) for x in range(r))
 print(b)
-for i in b:
-    print(i)
-    for e in i:
+a=''
+for i in range(0,len(b),n):
+    a=a+b[i:i+n]+'/'
+a=a[:-1]
+print(a)
+a=a.split('/')
+print(a)
+print('Результат фильтрации:')
+for i in a:
+    flag=0
+    for j in i:
+        if j >= 'A' and j <= 'Z':
+            flag=flag+1
+    if flag==0:
+        print(i)
+    
+
